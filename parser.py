a50799b33a7c98d288a77532f4b5db3fcd744d77
Michael Peter Christen <mc@yacy.net>
2023-11-07 00:37:49 +0100

    changing index metadata fields to yacy metadata

--- parser.py
+++ parser.py
@@ -6,6 +6,28 @@ def get_headline_level(line):
     """ Return the headline level based on the number of '#' characters """
     return line.count('#')
 
+def parse_metadata(lines):
+    """Parses the metadata at the beginning of the markdown file."""
+    metadata = {}
+    metadata_lines = []
+    inside_metadata = False
+    for line in lines:
+        if line.strip() == '---':
+            if inside_metadata:
+                # End of metadata section
+                break
+            else:
+                # Start of metadata section
+                inside_metadata = True
+        elif inside_metadata:
+            metadata_lines.append(line)
+    for line in metadata_lines:
+        key_value_match = re.match(r"(\w+):\s*(.*)", line)
+        if key_value_match:
+            key, value = key_value_match.groups()
+            metadata[key.lower()] = value.strip()
+    return metadata
+
 def parse(file_path, directory_name):
     chunks = []  # List to hold all chunks as dictionaries
     current_hierarchy = []  # To keep track of the hierarchy leading to the current headline
@@ -13,6 +35,17 @@ def parse(file_path, directory_name):
 
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.readlines()
+
+        # Parse metadata if it exists
+        metadata = {}
+        if content and content[0].strip() == '---':
+            metadata = parse_metadata(content)
+            # Skip past the metadata section to the rest of the content
+            content = content[content.index('---', 1) + 1:] if '---' in content else content
+        
+        # get the origslug metadata field
+        origslug = metadata.get('origslug', '')
+
         for line in content:
             if line.startswith('#'):
                 level = get_headline_level(line.strip())
@@ -22,8 +55,6 @@ def parse(file_path, directory_name):
                 paragraph_match = re.search(r'\u00a7\s*([\w\d]+)', title)
                 if paragraph_match:
                     current_paragraph = paragraph_match.group(1)
-                    # Remove the paragraph token from the title
-                    title = title.replace(paragraph_match.group(0), '').strip()
 
                 # Adjust the current_hierarchy to match the new level
                 if len(current_hierarchy) >= level:
@@ -32,21 +63,26 @@ def parse(file_path, directory_name):
                 
                 # Define the chunk with its description and paragraph number
                 description = current_hierarchy[:-1]  # The description is the hierarchy excluding the current title
+                descriptions = '\n'.join(description)
+
+                # compute a string from directory_name which has all umlaute replaced with an underscore
+                dirclean = directory_name.replace('ä', '_').replace('ö', '_').replace('ü', '_').replace('ß', '_').replace('Ä', '_').replace('Ö', '_').replace('Ü', '_')
+
                 chunk = {
+                    'url_s': 'https://www.gesetze-im-internet.de/' + origslug + '/__' + current_paragraph + '.html',
                     'directory_name': directory_name,
                     'title': title,
-                    'description': description,
+                    'description': descriptions + '\n' + title,
                     'paragraph': current_paragraph,
-                    'text': '',
+                    'text_t': descriptions + '\n' + title + '\n\n',
                     'level': level
                 }
-                chunks.append(chunk)
+                if (current_paragraph != ''): chunks.append(chunk)
             else:
                 if chunks:  # Append text to the last chunk
-                    chunks[-1]['text'] += line
+                    chunks[-1]['text_t'] += line
 
     return chunks
-
 
 def find_and_parse_index_md(start_path):
     all_chunks = []
@@ -58,12 +94,13 @@ def find_and_parse_index_md(start_path):
                 chunks = parse(full_path, directory_name)
                 all_chunks.extend(chunks)
 
-    # Convert the list of chunks to a JSON array
-    json_array = json.dumps(all_chunks, indent=4)
+    # Write the JSON lines to the filesystem
+    with open('bundestag_gesetze.jsonl', 'w', encoding='utf-8') as json_file:
+        for chunk in all_chunks:
+            # Write the prefix line
+            json_file.write(json.dumps({"index": {}}) + '\n')
+            # Write the chunk as a JSON line
+            json_file.write(json.dumps(chunk) + '\n')
 
-    # Write the JSON array to the filesystem
-    with open('parsed_content.json', 'w') as json_file:
-        json_file.write(json_array)
-
+# Run the function with the starting directory
 find_and_parse_index_md('.')
-
